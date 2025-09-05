3c1507e57bb860f87dcf172682c22e2e3aa5940a
Ashok16it <ashok16.it@gmail.com>
2025-09-05 23:34:58 +0530

    commited at the server

--- src/test/java/com/practice/test/SumPractice.java
+++ src/test/java/com/practice/test/SumPractice.java
@@ -2,4 +2,10 @@ package com.practice.test;
 
 public class SumPractice {
 
+  public static void main(String args[])
+  {
+
+    System.out.println("I have done this commit at github server")
+  }
+
 }
